e05c4ff0a734898b95fc0767be2e20d759fa2c33
matt <jmtooker@hotmail.com>
2021-10-23 06:51:33 -0500

    Intro to Redux

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Component } from "react/cjs/react.production.min";
-import { Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron, Nav } from "reactstrap";
+import { Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron, Nav, Button, Modal, ModalHeader,
+     ModalBody, Form, FormGroup, Input, Label } from "reactstrap";
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
@@ -8,18 +9,36 @@ class Header extends Component {
     //1st set up a constructor
     constructor(props){
         super(props);
+        
+        this.state = {
+            isNavOpen: false,
+            isModalOpen: false
+        };
         //bind the event handler to the component. It ensures that when toggleNav is called, 'this' keyword
         //inside it refers correctly to the component
         this.toggleNav = this.toggleNav.bind(this);
-        this.state = {
-            isNavOpen: false
-        };
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
     }
     //2nd set up a method that will handle when the navbar toggler button is clicked
     toggleNav () {
         this.setState({
             isNavOpen: !this.state.isNavOpen 
         });
+    }
+
+    toggleModal () {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen 
+        });
+    }
+
+    handleLogin(event) {
+        alert(`Username: ${this.username.value} Password: ${this.password.value}
+        Remeber: ${this.remember.checked}`);
+        this.toggleModal();
+        //prevents entire page from being rendered
+        event.preventDefault();
     }
 
     render() {
@@ -62,11 +81,39 @@ class Header extends Component {
                                         <i className="fa fa-address-card fa-lg" /> Contact Us
                                     </NavLink>
                                 </NavItem>
-
                             </Nav>
+                            <span className="navbar-text ml-auto">
+                                <Button outline onClick={this.toggleModal}>
+                                    <i className="fa fa-sign-in fa-lg" /> Login
+                                </Button>
+                            </span>
                     </Collapse>
                 </div>
                 </Navbar>
+
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username" 
+                                    //this passes the value of the input field with an callback function
+                                    innerRef={input => this.username = input} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="text" id="password" name="password" 
+                                     innerRef={input => this.password = input} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Input type="checkbox"  name="remember" 
+                                     innerRef={input => this.remember = input} /> Remember me
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="primary">Login</Button>
+                        </Form>
+                    </ModalBody>
+                </Modal>
             </React.Fragment>
         )
     }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,25 +9,20 @@ import Footer from './FooterCompont';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
 import About from "./AboutComponent";
-import { Switch, Route, Redirect } from 'react-router-dom';
-import {CAMPSITES} from '../shared/campsites';
-import {COMMENTS} from '../shared/comments';
-import {PARTNERS} from '../shared/partners';
-import {PROMOTIONS} from '../shared/promotions';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-class Main extends Component {  
-    //the state is stored here
-    constructor(props) {
-      super(props)
-      this.state = {
-        campsites: CAMPSITES,
-        comments: COMMENTS,
-        partners: PARTNERS, 
-        promotions: PROMOTIONS,
-        
-      };
+//get the state from Redux by setting up the below function(state is the argument)
+const mapStateToProps = state => {
+    return {
+        campsites: state.campsites,
+        comments: state.comments,
+        partners: state.partners, 
+        promotions: state.promotions
     }
-
+}
+class Main extends Component {  
+  
     render() {
         //locally scoped HomePage, only accesable inside MainComponent.js
         /*why arrow function? they inherit the this of their parent scope and 
@@ -37,10 +32,10 @@ class Main extends Component {
                 <Home 
                     /*the 0 is for the array index to pull that object out of the array then
                      pass it to home component as props */
-                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                    campsite={this.props.campsites.filter(campsite => campsite.featured)[0]}
                     /*the 0 is for the array index to pull that object out of the array */
-                    promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
-                    partner={this.state.partners.filter(partner => partner.featured)[0]}
+                    promotion={this.props.promotions.filter(promotion => promotion.featured)[0]}
+                    partner={this.props.partners.filter(partner => partner.featured)[0]}
                 />
                     
             )
@@ -49,11 +44,11 @@ class Main extends Component {
         const CampsiteWithId = ({match}) => {
             return (
                 <CampsiteInfo 
-                    campsite={this.state.campsites.filter(campsite => campsite.id ===
+                    campsite={this.props.campsites.filter(campsite => campsite.id ===
                     +match.params.campsiteId)[0]}
                     //with comments, we want the whole array not just a single comment so
                     //we dont have to use the zero index ([0]) here.  
-                    comments={this.state.comments.filter(comment => comment.campsiteId ===
+                    comments={this.props.comments.filter(comment => comment.campsiteId ===
                     +match.params.campsiteId)} />
             )
         }
@@ -62,13 +57,13 @@ class Main extends Component {
                 <Header />   {/*Header class in HeaderComponent.js */}
                 <Switch>
                     <Route path='/home' component={HomePage} />
-                    <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />}  />
+                    <Route exact path='/directory' render={() => <Directory campsites={this.props.campsites} />}  />
                     <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Route exact path='/contactus' component={Contact} />
                     <Route
                         exact 
                         path='/aboutus'
-                        render={() => <About partners={this.state.partners} />}
+                        render={() => <About partners={this.props.partners} />}  //replaced state with props
                         />
                     <Redirect to='/home' />
                 </Switch>
@@ -78,4 +73,4 @@ class Main extends Component {
     }
 }
 
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main));
