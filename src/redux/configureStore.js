d2930c7179a3aa4d76d91bd3879282bca58bd1ab
matt <jmtooker@hotmail.com>
2021-10-27 11:23:02 -0500

    React Redux Forms Revisited 2

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -5,6 +5,12 @@ import { Partners } from './partners';
 import { Promotions } from './promotions';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+//createForms is a helper function from the react/redux library. It
+//makes it easy to set up reducers to update the state whenever new 
+//form values are submitted. It is designed to be used with this 
+//redux function combineReducers
+import { createForms } from 'react-redux-form';
+import { InitialFeedback } from './forms';
 
 
 export const ConfigureStore = () => {
@@ -14,6 +20,13 @@ export const ConfigureStore = () => {
             comments: Comments,
             partners: Partners,
             promotions: Promotions
+            //we'll pass createForms to combineReducers as one of the arguments
+            //using the spread syntax and giving it an argument that contains a
+            //model name for the entire form, we'll call feedbackForm. We will 
+            //give it the InitialFeedback object for the inital form state.
+            ...createForms({
+                feedbackForm: InitialFeedback
+            })
         }),
         applyMiddleware(thunk, logger)
     );
